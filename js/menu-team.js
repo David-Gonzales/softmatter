4caf7af1c181bb3d55aa6a1c9e2c36a6dee1960d
joisromero <joseluis0651@gmail.com>
2021-04-06 16:18:53 -0500

    Se termino la pagina de Team

---
+++ js/menu-team.js
@@ -0,0 +1,22 @@
+let open = document.getElementById('abrir-menu-team');
+let menuTeam = document.getElementById('nav-menu-item');
+let close = true;
+
+function aperturaMenuTeam() {
+
+    if (close) {
+        close = false;
+        menuTeam.style.transition = '3s';
+        menuTeam.style.display = 'block';
+    } else {
+        close = true;
+        menuTeam.style.display = 'none';
+    }
+
+}
+
+
+
+open.addEventListener('click', function() {
+    aperturaMenuTeam();
+})
